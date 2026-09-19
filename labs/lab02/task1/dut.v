783517779a0cc61164ae38f088db906281c362ac
f20251274-spec <319413372+f20251274-spec@users.noreply.github.com>
2026-09-19 14:52:00 +0000

    lab02 task1

--- labs/lab02/task1/dut.v
+++ labs/lab02/task1/dut.v
@@ -1,5 +1,8 @@
 // dut.v
 
+// Top-level wrapper to test either implementation.
+// Uncomment exactly ONE instantiation at a time.
+
 module DUT (
     input  I0,
     input  I1,
@@ -9,18 +12,18 @@ module DUT (
 
     // ---- Option 1: Dataflow version ----
     mux_df U1 (
-        .I0(I0),
-        .I1(I1),
-        .S(S),
-        .Y(Y)
+        .I0 (I0),
+        .I1 (I1),
+        .S  (S),
+        .Y  (Y)
     );
 
     // ---- Option 2: Behavioral version ----
     // mux_beh U1 (
-    //     .I0(I0),
-    //     .I1(I1),
-    //     .S(S),
-    //     .Y(Y)
+    //     .I0 (I0),
+    //     .I1 (I1),
+    //     .S  (S),
+    //     .Y  (Y)
     // );
 
 endmodule
